6e2ccc4278c13d3e4f114a7ed3000f3ca76806e8
LittleStarZzz1 <yx140513@163.com>
2020-03-29 20:26:10 +0800

    Lib_Test

---
+++ Lib_test/LIB/child.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void Print_Child()
+{
+    printf("i am child!\n");
+}
---
+++ Lib_test/LIB/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "child.h"
+
+int main()
+{
+    Print_Child();
+    return 0;
+}
